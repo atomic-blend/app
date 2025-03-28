d907d28c0ea959092be7e81c28e560326cc8b035
Brandon Guigo <brandon.guigo@gmail.com>
2025-03-28 10:36:20 +0100

    fix: configure firebase prod

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,19 +1,18 @@
 // firebase_options.dart
 
+import 'package:app/firebase_options_prod.dart' as prod;
+
 import 'firebase_options_dev.dart' as dev;
 
 import 'package:firebase_core/firebase_core.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    const flavor = String.fromEnvironment('FLAVOR');
+    const flavor = String.fromEnvironment('APP_ENV');
 
     switch (flavor) {
       case 'prod':
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for prod - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return prod.DefaultFirebaseOptions.currentPlatform;
       case 'dev':
       default:
         return dev.DefaultFirebaseOptions.currentPlatform;
