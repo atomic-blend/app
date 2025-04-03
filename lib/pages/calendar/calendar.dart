900ca133e3c3ca0c83cea96b9fa78d0c839b00f4
Brandon Guigo <brandon.guigo@gmail.com>
2025-04-03 11:01:41 +0200

    feat: display device calendar events without calendar color

--- lib/pages/calendar/calendar.dart
+++ lib/pages/calendar/calendar.dart
@@ -5,6 +5,8 @@ import 'package:app/entities/tasks/tasks.entity.dart';
 import 'package:app/pages/calendar/appointment_data_source.dart';
 import 'package:app/utils/constants.dart';
 import 'package:app/utils/shortcuts.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:device_calendar/device_calendar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -64,32 +66,43 @@ class _CalendarState extends State<Calendar> {
               dateTextStyle: getTextTheme(context).bodyMedium,
             ),
           ),
-          dataSource: _getTasks(taskState.tasks ?? []),
+          dataSource: _getDataSource(
+              taskState.tasks ?? [], deviceCalendarState.deviceCalendar ?? []),
           appointmentBuilder:
               (BuildContext context, CalendarAppointmentDetails details) {
-            return Container(
-              decoration: BoxDecoration(
-                color: details.appointments.first.color,
-                borderRadius: BorderRadius.circular(6),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "${details.appointments.first.subject}",
-                    style: getTextTheme(context).bodyMedium!.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  // Text(
-                  //   "${Jiffy.parseFromDateTime(details.appointments.first.startTime).Hm.toString()} - ${Jiffy.parseFromDateTime(details.appointments.first.endTime).Hm.toString()}",
-                  //   style: getTextTheme(context).bodySmall!,
-                  // ),
-                ],
-              ),
-            );
+            return LayoutBuilder(builder: (context, constraints) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: details.appointments.first.color,
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: details.appointments.first.isAllDay ||
+                              details.appointments.first.notes == "" ||
+                              details.appointments.first.notes == null
+                          ? constraints.maxHeight
+                          : constraints.maxHeight * 0.5,
+                      child: AutoSizeText(
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        "${details.appointments.first.subject}",
+                        style: getTextTheme(context).bodyMedium!.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ),
+                    // Text(
+                    //   "${Jiffy.parseFromDateTime(details.appointments.first.startTime).Hm.toString()} - ${Jiffy.parseFromDateTime(details.appointments.first.endTime).Hm.toString()}",
+                    //   style: getTextTheme(context).bodySmall!,
+                    // ),
+                  ],
+                ),
+              );
+            });
           },
         );
       });
@@ -122,6 +135,37 @@ class _CalendarState extends State<Calendar> {
           ),
         );
       }
+    }
+    return AppointmentDataSource(appointments);
+  }
+
+  AppointmentDataSource _getDeviceEvents(List<DeviceCalendar> calendars) {
+    final List<Appointment> appointments = <Appointment>[];
+    for (DeviceCalendar calendar in calendars) {
+      for (Event event in calendar.events) {
+        appointments.add(
+          Appointment(
+            startTime: event.start!.toLocal(),
+            endTime: event.end!.toLocal(),
+            subject: event.title ?? "No title",
+            color: getTheme(context).primary.withValues(alpha: 0.2),
+            notes: event.description,
+            isAllDay: event.allDay ?? false,
+          ),
+        );
+      }
+    }
+    return AppointmentDataSource(appointments);
+  }
+
+  AppointmentDataSource _getDataSource(
+      List<TaskEntity> tasks, List<DeviceCalendar> calendars) {
+    final List<Appointment> appointments = <Appointment>[];
+    for (final a in _getTasks(tasks).appointments ?? []) {
+      appointments.add(a);
+    }
+    for (final a in _getDeviceEvents(calendars).appointments ?? []) {
+      appointments.add(a);
     }
     return AppointmentDataSource(appointments);
   }
