fd6986d24d698d917395166bd94cf7ef6622f1cc
Brandon Guigo <brandon.guigo@gmail.com>
2025-04-09 12:17:58 +0200

    fix: add spaceer for headline in habit detail

--- lib/pages/habits/habit_detail.dart
+++ lib/pages/habits/habit_detail.dart
@@ -58,6 +58,7 @@ class _HabitDetailState extends State<HabitDetail> {
                           fontWeight: FontWeight.bold,
                         ),
                   ),
+                  const Spacer(),
                   GestureDetector(
                     onTap: () {
                       widget.onEdit?.call();
