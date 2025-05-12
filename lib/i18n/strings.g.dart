f99035ea1fc07ce5d606b975706a57ceec9b76ce
Brandon Guigo <brandon.guigo@gmail.com>
2025-05-12 16:53:12 +0200

    fix: be able to run on mac OS with firebase messaging
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 1055 (527 per locale)
 ///
-/// Built on 2025-05-12 at 09:29 UTC
+/// Built on 2025-05-12 at 14:39 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
