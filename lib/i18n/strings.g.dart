b5ecdb78415454c279157960d18864e1090e3337
Brandon Guigo <brandon.guigo@gmail.com>
2025-05-12 11:30:59 +0200

    fix: email validation
    make sure the email is not, before sending the request to the backend :
    - empty
    - equal as current email
    
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -4,9 +4,9 @@
 /// To regenerate, run: `dart run slang`
 ///
 /// Locales: 2
-/// Strings: 1051 (525 per locale)
+/// Strings: 1055 (527 per locale)
 ///
-/// Built on 2025-05-12 at 08:45 UTC
+/// Built on 2025-05-12 at 09:29 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
