ae7852ef7cb31f8df5307f383ca7bfa17a1f52a5
Brandon Guigo <brandon.guigo@gmail.com>
2025-05-16 19:33:31 +0200

    feat: add title on eisenhower cards
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/pages/eiseinhower/eisenhower.dart
+++ lib/pages/eiseinhower/eisenhower.dart
@@ -2,6 +2,7 @@ import 'package:app/entities/tasks/tasks.entity.dart';
 import 'package:app/i18n/strings.g.dart';
 import 'package:app/utils/constants.dart';
 import 'package:app/utils/shortcuts.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EisenhowerMatrix extends StatelessWidget {
@@ -32,15 +33,15 @@ class EisenhowerMatrix extends StatelessWidget {
                   children: [
                     buildEisenhowerCard(
                         context: context,
+                        priority: 3,
                         title: "Urgent & Important",
-                        accentColor: Colors.red,
                         filter: (task) {
                           return task.where((element) => element.priority == 3);
                         }),
                     buildEisenhowerCard(
                         context: context,
+                        priority: 2,
                         title: "Not Urgent & Important",
-                        accentColor: Colors.red,
                         filter: (task) {
                           return task.where((element) => element.priority == 2);
                         }),
@@ -53,18 +54,19 @@ class EisenhowerMatrix extends StatelessWidget {
                   children: [
                     buildEisenhowerCard(
                         context: context,
+                        priority: 1,
                         title: "Urgent & Unimportant",
-                        accentColor: Colors.red,
                         filter: (task) {
                           //TODO: add filter for overdue tasks
                           return task.where((element) => element.priority == 1);
                         }),
                     buildEisenhowerCard(
                         context: context,
+                        priority: null,
                         title: "Not Urgent & Unimportant",
-                        accentColor: Colors.red,
                         filter: (task) {
-                          return task.where((element) => element.priority == 0);
+                          return task
+                              .where((element) => element.priority == null);
                         }),
                   ],
                 ),
@@ -79,11 +81,12 @@ class EisenhowerMatrix extends StatelessWidget {
   Widget buildEisenhowerCard({
     required BuildContext context,
     required String title,
-    required Color accentColor,
+    required int? priority,
     required Function(List<TaskEntity>) filter,
   }) {
     return Expanded(
       child: Container(
+        padding: EdgeInsets.all($constants.insets.xs),
         decoration: BoxDecoration(
           color: getTheme(context).surfaceContainer,
           borderRadius: BorderRadius.circular($constants.insets.sm),
@@ -92,7 +95,40 @@ class EisenhowerMatrix extends StatelessWidget {
           children: [
             Row(
               children: [
-                Text(title),
+                SizedBox(
+                  width: $constants.insets.xs,
+                ),
+                Container(
+                  width: 25,
+                  height: 25,
+                  decoration: BoxDecoration(
+                    color: priority == null || priority == 0
+                        ? Colors.grey
+                        : priority == 1
+                            ? Colors.blueAccent
+                            : priority == 2
+                                ? Colors.deepOrangeAccent
+                                : Colors.red,
+                    borderRadius: BorderRadius.circular($constants.insets.sm),
+                  ),
+                  child: Icon(
+                    CupertinoIcons.flag_fill,
+                    size: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: $constants.insets.xs,
+                ),
+                Flexible(
+                  child: Text(
+                    title,
+                    style: getTextTheme(context).bodyMedium!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
               ],
             )
           ],
