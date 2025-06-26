76a4581a7c81fdd1c8af1b8a9fd471a7bb686d47
Brandon Guigo <brandon.guigo@gmail.com>
2025-06-26 19:27:29 +0200

    feat: widget extension on flutter finally working
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

---
+++ ios/todayTaskWidget/todayTaskWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  todayTaskWidgetBundle.swift
+//  todayTaskWidget
+//
+//  Created by Brandon Guigo on 26/06/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct todayTaskWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        todayTaskWidget()
+    }
+}
